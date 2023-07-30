fd3117a59653055418a126f6ab72494b189ee40e
niceyraiyani <82303372+niceyraiyani@users.noreply.github.com>
2023-07-29 21:11:07 -0400

    Add files via upload

---
+++ project 2/proj2_template.py
@@ -0,0 +1,28 @@
+#----------------------------------------
+#  CSC 315 / 615 Spring 2023
+#  Project 2 Flower Species Analysis
+#
+#  <<Write your name here>>
+#----------------------------------------
+
+
+%matplotlib inline
+import matplotlib.pyplot as plt
+plt.style.use('seaborn-whitegrid')
+import numpy as np
+import pandas as pd
+
+
+
+#----
+# Define your functions here
+#----
+
+#----
+# Read the Iris data
+#----
+df = pd.read_csv("iris.csv")
+
+#----
+# Run your main code here
+#----
